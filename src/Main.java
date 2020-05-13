834082b0cdebd00f1d031666547f12783f7d5fdf
sergei Shapovalov <senishapovalov@gmail.com>
2020-05-13 22:02:42 +0300

    Correctons

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 2_000_000_000;
+        long balance = 2_000_000_000;
         System.out.println("balance: " + balance);
         int transfer = 500_000_000;
         System.out.println("transfer: " + transfer);
-        int total = balance - transfer;
+        long total = balance + transfer;
         System.out.println("total: " + total);
     }
 }
